9fa6f8eca67419af7879d9ae68feee1044a21d2e
Nathan Lefetey <lefetey.nathan@gmail.com>
2023-03-17 09:08:18 +0100

    :construction: UPDATE forms
    Add form step

--- participation.php
+++ participation.php
@@ -22,70 +22,71 @@
 </head>
 <body>
     <form method="POST">
-        <label for="lastname">Nom</label>
-            <input type="text" name="lastname" id="lastname" placeholder="Nom">
-        <label for="firstname">Prénom</label>
-            <input type="text" name="firstname" id="firstname" placeholder="Prénom">
-        <label for="email">Email</label>
-            <input type="email" name="email" id="email" placeholder="Email">
-        <label for="phone">Téléphone</label>
-            <input type="tel" name="phone" id="phone" placeholder="Téléphone">
-        <button type="submit">Passer à l'étape suivant</button>
-    </form>
-    <form method="POST">
-        <label for="title">Titre du produit</label>
-            <input type="text" name="title" id="title" placeholder="Titre du produit">
-        <label for="description">Description du produit</label>
-            <textarea name="description" id="description" cols="30"></textarea>
-        <button type="submit">Passer à l'étape suivante</button>
-    </form>
-    <form method="POST">
-        <select name="category">
-            <?php
+        <div id="step-1" class="owner-infos">
+            <label for="lastname">Nom</label>
+                <input type="text" name="lastname" id="lastname" placeholder="Nom">
+            <label for="firstname">Prénom</label>
+                <input type="text" name="firstname" id="firstname" placeholder="Prénom">
+            <label for="email">Email</label>
+                <input type="email" name="email" id="email" placeholder="Email">
+            <label for="phone">Téléphone</label>
+                <input type="tel" name="phone" id="phone" placeholder="Téléphone">
+
+            <button type="button" value="1" id="step">Passer à l'étape suivante</button>
+        </div>
+
+        <div id="step-2" class="product-infos" style="display: none">
+            <label for="title">Titre du produit</label>
+                <input type="text" name="title" id="title" placeholder="Titre du produit">
+            <label for="description">Description du produit</label>
+                <textarea name="description" id="description" cols="30"></textarea>
+
+            <button type="button" value="2" id="step">Passer à l'étape suivante</button>
+        </div>
+
+        <div id="step-3" class="product-details" style="display: none">
+            <select name="category">
+                <?php
                 foreach ($categories as $category) : ?>
                     <option value="<?= $category['id'] ?>"><?= $category['title'] ?></option>
                 <?php endforeach;
-            ?>
-        </select>
-        <label for="brand">Marque</label>
-            <input type="text" name="brand" id="marque" placeholder="Marque">
+                ?>
+            </select>
 
-        <select name="color">
-            <?php
+            <label for="brand">Marque</label>
+                <input type="text" name="brand" id="marque" placeholder="Marque">
+
+            <select name="color">
+                <?php
                 foreach ($colors as $color) : ?>
                     <option value="<?= $color['id'] ?>"><?= $color['title'] ?></option>
                 <?php endforeach;
-            ?>
-        </select>
-        <select name="matter">
-            <?php
+                ?>
+            </select>
+            <select name="matter">
+                <?php
                 foreach ($matters as $matter) : ?>
                     <option value="<?= $matter['id'] ?>"><?= $matter['title'] ?></option>
                 <?php endforeach;
-            ?>
-        </select>
-        <select name="state">
-            <?php
+                ?>
+            </select>
+            <select name="state">
+                <?php
                 foreach ($states as $state) : ?>
                     <option value="<?= $state['id'] ?>"><?= $state['title'] ?></option>
                 <?php endforeach;
-            ?>
-        </select>
-        <select name="size">
-            <?php
+                ?>
+            </select>
+            <select name="size">
+                <?php
                 foreach ($sizes as $size) : ?>
                     <option value="<?= $size['id'] ?>"><?= $size['title'] ?></option>
                 <?php endforeach;
-            ?>
-        </select>
+                ?>
+            </select>
 
-        <button type="submit">Effectuer mon don !</button>
-    </form>
-
-    <form method="POST" id="test-form">
-        <input type="text" name="test-name">
-        <input type="text" name="test-email">
-        <button type="submit">Submit</button>
+            <button type="submit">Effectuer mon don !</button>
+        </div>
     </form>
 </body>
 </html>
