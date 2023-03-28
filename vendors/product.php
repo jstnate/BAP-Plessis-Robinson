004d831b45be15e8abb9db4df1e403d43f75109c
Arthur Prudhomme <arthurprudhommemail06@gmail.com>
2023-03-28 10:37:00 +0200

    :lipstick: sidebar filters & sorting

--- vendors/product.php
+++ vendors/product.php
@@ -28,10 +28,6 @@ array_push($sortings, (isset($_GET['orderD'])) ? ($_GET['orderD']) : (null));
 //var_dump($globalFilterArray);
 $datas = $connection->getData($globalFilterArray, (isset($_GET['query'])) ? ($_GET['query']) : (null), $sortings);
 
-foreach ($datas as $data){
-    echo $data['owner_email'].' - '.$data['title'].' - '.$data['description'].' - '.$data['categories'].' - '.$data['colors'].' - '.$data['matters'].' - '.$data['states'].' - '.$data['sizes'].' - '.$data['publication'].'<br>';
-}
-
 if ($_POST) {
     $parameters = '';
     $argsI = 0;
@@ -55,31 +51,57 @@ if ($_POST) {
 }
 
 ?>
-<html>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="./styles/build.css">
+    <title>Document</title>
+</head>
 <body>
 <form method="post">
     <input type="text" name="query"><br><br>
 
-    <?php
-    foreach ($allFilters as $filter) {
-        $filterValues = $connection->getSorting($filter);
-        echo $filter.'<br>';
-        foreach($filterValues as $index){
-            echo '<input type="checkbox" name="'.str_replace(' ','_',$index['title']).'">'.$index['title'].'<br>';
+    <div class="sidenav">
+        <div class="filters">
+            <?php
+            foreach ($allFilters as $filter) {
+                $filterValues = $connection->getSorting($filter);
+                echo '<div><h4>'.$filter.'</h4>';
+                foreach($filterValues as $index){
+                    echo '<p><input type="checkbox" name="'.str_replace(' ','_',$index['title']).'">'.$index['title'].'</p>';
+                }
+                echo '</div>';
+            }
+            ?>
+        </div>
+        <div class="sorting">
+            <div>
+                <h4>Name</h4>
+                <p><input type="radio" name="order_name" value="DESC">DESC</p>
+                <p><input type="radio" name="order_name" value="ASC">ASC</p>
+                <p><input type="radio" name="order_name" value="" checked>None</p>
+            </div>
+            <div>
+                <h4>Date</h4>
+                <p><input type="radio" name="order_date" value="DESC">DESC</p>
+                <p><input type="radio" name="order_date" value="ASC">ASC</p>
+                <p><input type="radio" name="order_date" value="" checked>None</p>
+            </div>
+        </div>
+<!--        <input type="submit" value="Search">-->
+    </div>
+    <div class="main">
+        <?php
+        foreach ($datas as $data){
+            echo $data['owner_email'].' - '.$data['title'].' - '.$data['description'].' - '.$data['categories'].' - '.$data['colors'].' - '.$data['matters'].' - '.$data['states'].' - '.$data['sizes'].' - '.$data['publication'].'<br>';
         }
-    }
-    ?>
+        ?>
+    </div>
 
-    <p>Name</p>
-    <input type="radio" name="order_name" value="DESC">DESC
-    <input type="radio" name="order_name" value="ASC">ASC
-    <input type="radio" name="order_name" value="" checked>None
-    <p>Date</p>
-    <input type="radio" name="order_date" value="DESC">DESC
-    <input type="radio" name="order_date" value="ASC">ASC
-    <input type="radio" name="order_date" value="" checked>None
-    <p></p>
-    <input type="submit" value="Search">
 </form>
 
 </body>
